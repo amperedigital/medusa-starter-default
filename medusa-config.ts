efa67e840d901cbb5422deb29a097ec2e656ed68
Andrew Crowe <andrew@amperedigital.ca>
2025-04-06 19:41:57 -0400

    Fix defineConfig import

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,13 +1,13 @@
 import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
-loadEnv(); // Always load env vars
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL!,
     redisUrl: process.env.REDIS_URL!,
     http: {
-      storeCors: process.env.STORE_CORS || "", // fallback empty string
+      storeCors: process.env.STORE_CORS || "",
       adminCors: process.env.ADMIN_CORS || "",
       authCors: process.env.AUTH_CORS || "",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
