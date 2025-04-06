919b82e10fa2e58c68d60c7027ed5a25aa9971b9
Andrew Crowe <andrew@amperedigital.ca>
2025-04-06 17:29:59 -0400

    Fix Redis connection and rebuild

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,7 +5,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL, // important for workflows!
+    redisUrl: process.env.REDIS_URL, // for event bus etc
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -15,6 +15,12 @@ export default defineConfig({
     },
   },
   modules: {
-    // Do NOT put workflows here
+    workflows: {
+      resolve: "@medusajs/workflows",
+      options: {
+        redisUrl: process.env.REDIS_URL,  // <-- your redis URL
+        redisTls: true,                   // <-- tell it we need TLS
+      },
+    },
   },
 })
