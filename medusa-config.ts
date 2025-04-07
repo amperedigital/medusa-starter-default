2e53bd396089440ab05018f205d7ad6e62588897
Andrew Crowe <andrew@amperedigital.ca>
2025-04-06 21:42:32 -0400

    Removed workflows for stable deployment

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,7 +5,7 @@ loadEnv(process.env.NODE_ENV || "development", process.cwd());
 export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL!,
-    redisUrl: process.env.REDIS_URL!,
+    redisUrl: process.env.REDIS_URL!, // ✅ your Railway URL here
     http: {
       storeCors: process.env.STORE_CORS || "",
       adminCors: process.env.ADMIN_CORS || "",
@@ -13,16 +13,17 @@ export default defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
+    redisOptions: {
+      tls: {},
+    },
   },
   modules: {
-    // ❌ REMOVE workflows
     stock_location: false,
     inventory: false,
     pricing: false,
     product_variant: false,
     tax: false,
     shipping_profile: false,
-    shipping_option: false
-    },
- 
+    shipping_option: false,
+  },
 });
